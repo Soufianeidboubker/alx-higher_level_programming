8c341c24a1f5b1ab23bafa918ccf355e3559cb38
soufianeidboubker <soufianeidboubker@gmail.com>
2023-08-16 07:44:01 -0700

    updates

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.cp()
+    new_dir = a_dictionary.copy()
     list_keys = list(new_dir.keys())
 
     for s in list_keys:
