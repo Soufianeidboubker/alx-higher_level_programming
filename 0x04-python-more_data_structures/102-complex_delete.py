53952e08bf1645727d4a00f1fb7dd301f39f5614
soufianeidboubker <soufianeidboubker@gmail.com>
2023-08-16 08:08:11 -0700

    updates

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_keys = list(a_dictionary.keys())
+
+    for val_dic in list_keys:
+        if val == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+    return (a_dictionary)
