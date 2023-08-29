474330d719fe74abcf6f60c224328ac59dd45506
soufianeidboubker <soufianeidboubker@gmail.com>
2023-08-29 22:02:12 +0000

    updates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
@@ -51,8 +52,8 @@ class Square:
             print("")
             return
 
-        [print("") for s in range(0, self.__position[1])]
-        for s in range(0, self.__size):
-            [print(" ", end="") for k in range(0, self.__position[0])]
-            [print("#", end="") for j in range(0, self.__size)]
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
