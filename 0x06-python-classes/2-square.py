621f006366a4966fd51a3565b88023b2986a6cb0
soufianeidboubker <soufianeidboubker@gmail.com>
2023-08-29 20:33:46 +0000

    updates

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a nw Square.
+        """Initialize a new Square.
 
         """
         if not isinstance(size, int):
