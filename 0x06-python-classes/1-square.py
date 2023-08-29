27f1cd9adfd07700ec89baf39489c0516a67545e
soufianeidboubker <soufianeidboubker@gmail.com>
2023-08-29 19:43:47 +0000

    updates

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"Define a class Square."
+
+class Square:
+    "Represent a square."
+
+    def __init__(self, size):
+        "Initialize a nw Square.
+
+        Args:
+            size (int): The size of the nw square.
+        "
+        self.__size = size
